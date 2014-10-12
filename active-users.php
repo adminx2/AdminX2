6afde50470e93873c1e421c49b3af61054445de7
adminx2 <admnx2@gmail.com>
2014-10-12 15:00:20 -0400

    Added comments.

--- active-users.php
+++ active-users.php
@@ -1,3 +1,7 @@
+<?php 
+// This is just a demonstration file. It is used as a request URL to update a div with a random number to demonstrate
+// the ability to auto update on a timer or update with a refresh button. 
+?>
 <p>
     There are currently <strong><?=rand(1,5000);?></strong> active users on your site.
 </p>    
